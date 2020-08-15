a0b33a40527473751ca1a136b43cf3963ac00743
Abubaker  Shekhani <abubakershekhani@gmail.com>
2020-08-15 13:47:24 +0500

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,9 @@
 const CACHE_NAME = "version-1";
-const urlsToCache = [ 'index.html' ];
+const urlsToCache = [ 'index.html',
+                      '/static/css/main.5f361e03.chunk.css',
+                      '/static/js/2.b4a7e716.chunk.js',
+                    '/static/js/main.0493d116.chunk.js',
+                  '/logo192.png'];
 
 const self = this;
 
